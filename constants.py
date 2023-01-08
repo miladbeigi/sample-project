7f7558c9dfa78f28c389e891c07b87ef2c196958
miladbeigi <milad.be@gmail.com>
2023-01-08 14:11:29 +0100

    fix custom logging

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ RUN_PERIOD_SECONDS = 10
 WAIT_FOR_DATABASE_SECONDS = 5
 BUF_SIZE = 65536
 YAML_FILE_NAME = 'permissions.yaml'
-LOG_FILE_PATH = 'update-permissions.log'
+LOG_FILE_PATH = './logs/update-permissions.log'
